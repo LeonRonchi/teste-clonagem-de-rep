d96cbe67760c73c254380733e0324cdbfd7b7b00
LeonRonchi <leonbronchi@gmail.com>
2023-09-18 09:01:40 -0300

    some updates

--- src/telasCadastro/tabelas/TabelaClientes.jsx
+++ src/telasCadastro/tabelas/TabelaClientes.jsx
@@ -12,6 +12,13 @@ export default function TabelaClientes(props) {
             );
     }
 
+    function editarCliente(cliente){
+        props.setClienteParaEdicao(cliente);
+        props.setModoEdicao(true);
+        props.exibirFormulario(true);
+    }
+
+    
     return (
         <Container>
             <Button type="button" onClick={() => {
@@ -62,7 +69,7 @@ export default function TabelaClientes(props) {
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
                                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
                                     </svg>
-                                </Button>
+                                </Button> {' '}
 
                                     <Button variant="warning">
                                         <svg xmlns="http://www.w3.org/2000/svg"
